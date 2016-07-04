2d161d3e8c13a4b46bf9470b65d41f5e171d3f0e
Atul Varma <varmaa@gmail.com>
2016-07-04 11:14:30 -0400

    Add a few tests.

--- pode/tests.py
+++ pode/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+    def test_home_has_login_link_when_logged_out(self):
+        res = self.client.get('/')
+        self.assertContains(res, 'Login via GitHub')
+        self.assertEqual(res.status_code, 200)
+
+    def test_home_has_logout_link_when_logged_in(self):
+        user = User.objects.create_user(username='foo')
+        self.client.force_login(user)
+        res = self.client.get('/')
+        self.assertContains(res, 'Logout foo')
+        self.assertEqual(res.status_code, 200)
