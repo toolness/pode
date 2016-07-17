20ca273c85198246cee7dd8547281a075530fe34
Atul Varma <varmaa@gmail.com>
2016-07-17 07:59:17 -0400

    Add some syntax help.

--- pode/static/edit_user_code.js
+++ pode/static/edit_user_code.js
@@ -20,11 +20,60 @@ $(function() {
     say(msg);
   }
 
-  function getLineNumber() {
-    var index = textarea.selectionStart;
+  function getLineNumber(index) {
     var substr = textarea.value.substring(0, index);
 
     return substr.split('\n').length;
+  }
+
+  function getSyntaxHelp(html, index) {
+    var result = Slowparse.HTML(document, html, {
+      disallowActiveAttributes: true
+    });
+    var node, tagInfo;
+
+    function isInside(tagInfo) {
+      if (!tagInfo) return false;
+      return index > tagInfo.start && index < tagInfo.end;
+    }
+
+    function isInContent(parseInfo) {
+      if (!(parseInfo.openTag && parseInfo.closeTag)) return false;
+      return index >= parseInfo.openTag.end &&
+             index <= parseInfo.closeTag.start;
+    }
+
+    function descend(parent) {
+      var tagName;
+      var descendants;
+
+      for (var i = 0; i < parent.childNodes.length; i++) {
+        node = parent.childNodes[i];
+        tagName = node.nodeName;
+        if (node.parseInfo) {
+          if (isInside(node.parseInfo.openTag)) {
+            if (node.parseInfo.closeTag) {
+              return "inside an opening " + tagName + " tag";
+            } else {
+              return "inside a " + tagName + " tag";
+            }
+          } else if (isInside(node.parseInfo.closeTag)) {
+            return "inside a closing " + tagName + " tag";
+          } else if (isInContent(node.parseInfo)) {
+            descendants = descend(node);
+            return descendants + " inside a " + tagName + " element";
+          }
+        }
+      }
+
+      return '';
+    }
+
+    if (!result.document) {
+      return '';
+    }
+
+    return descend(result.document);
   }
 
   if (speechSupported) {
@@ -39,9 +88,22 @@ $(function() {
   }
 
   $(document).keydown(function(e) {
+    var index, help, syntaxHelp;
+
     if (e.key == 'h' && e.ctrlKey) {
       e.preventDefault();
-      showHelp('Cursor is on line ' + getLineNumber() + '.');
+
+      index = textarea.selectionStart;
+      syntaxHelp = getSyntaxHelp(textarea.value, index);
+      help = 'Cursor is on line ' + getLineNumber(index);
+
+      if (syntaxHelp) {
+        help += ', ' + syntaxHelp;
+      }
+
+      help += '.';
+
+      showHelp(help);
     }
   });
 });
